2fc6814954fe4a670cbcecc46f7364fdad854892
Data <110648069+data0504@users.noreply.github.com>
2022-09-03 09:54:52 +0800

    Add files via upload
    * 第八行，請更改為本端絕對路徑、相對路徑。

---
+++ PhoneNunber.py
@@ -0,0 +1,31 @@
+from random import sample
+
+class randomPhoneNumber():
+    def __init__(self,rows, limit, country):
+        self.rows= rows
+        self.Limit= limit
+        self.country= country
+        self.destFile= rf'homeworks\1.PhoneNumber_SmallTest\RandomPhoneNumber_{country}_{rows}rows.txt'
+
+    def number(self):
+        _list= [0,1,2,3,4,5,6,7,8,9]
+        listRandom= sample(_list,self.Limit)
+        return listRandom
+    
+    def write(self):
+        with open(self.destFile, 'w') as f:
+            for j in range(self.rows):
+                resolve= ''
+                resolve+= str(self.country)+ ' '+f'9'
+
+                for i in self.number():
+                    resolve+= f'{i}'
+                print(resolve)
+                f.write(''.join(resolve+ "\n"))
+        f.close()
+
+rows= 10
+numberLimit= 8
+countryCode= '+886'
+randomN= randomPhoneNumber(rows, numberLimit, countryCode)
+randomN.write()
